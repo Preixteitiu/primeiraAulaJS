81f1b848666525d091e66a1d9e26f68823efcf5c
Preixteitiu <isaquesarev@gmail.com>
2026-04-01 21:21:14 -0300

    atividade biblioteca

--- objetos/classes/biblioteca.js
+++ objetos/classes/biblioteca.js
@@ -1,37 +1,45 @@
-class Livro{
-    constructor(titulo, autor){
-        this.titulo = titulo
-        this.autor = autor
-        this.disponivel = true
-    }
-
-    
+class Livro {
+  constructor(titulo, autor) {
+    this.nome = titulo;
+    this.autor = autor;
+  }
 }
 
+class Biblioteca {
+  constructor() {
+    this.livros = [];
+    this.disponivel = true;
+  }
 
-class Biblioteca{
-    constructor(){
-        this.livros = []
+  adicionarLivros(titulo) {
+    this.livros.push(titulo);
+    console.log(`O item ${titulo.nome} foi adicionado`);
+  }
+
+  listarLivros() {
+    return this.livros.length
+  }
+
+  seDisponivel(titulo) {
+    if (this.disponivel) {
+      console.log(`O ${titulo.nome} está disponível`);
+    } else {
+      console.log(`O livro ${titulo.nome} não está disponível `);
     }
+  }
 
-    adicionarLivros(livro){
-        this.livros.push(livro)
-        console.log(`O item ${livro.nome}`)
-    }
-
-    listarLivros(){
-        let disponivel = this.disponivel
-        if (this.livros.length > 0)
-        return this.livros.forEach((titulo, disponivel)=> {
-        `O ítem ${titulo.nome} está ${disponivel} `
-    })
-    }
-
-    seDisponivel(titulo){
-        if(this.titulo = this.disponivel)
-            console.log(`O título ${titulo.nome} está disponível`)
-        else{
-            console.log(`O título ${titulo.nome} não está disponível`)
-        }
+  emprestarLivros(titulo){
+    this.livros.pop()
+    const emLeitura = !this.disponivel
+    console.log(`O item ${titulo.nome} foi emprestado com sucesso`)
+    return emLeitura
     }
 }
+
+const livro1 = new Livro(`Piratas do Caribe`, `Jack Sparrow`);
+
+const prateleira = new Biblioteca(`SEBO`);
+
+prateleira.adicionarLivros(livro1);
+prateleira.seDisponivel(livro1);
+prateleira.listarLivros()
