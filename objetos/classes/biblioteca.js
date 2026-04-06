cabaadc6caa4cfd6d27a6e840e40f8dc6cb95f96
Preixteitiu <isaquesarev@gmail.com>
2026-04-06 19:49:18 -0300

    atividade

--- objetos/classes/biblioteca.js
+++ objetos/classes/biblioteca.js
@@ -20,10 +20,6 @@ class Biblioteca {
     }
   }
 
-  listarLivros() {
-    return this.livros.length;
-  }
-
   seDisponivel(titulo) {
     if (this.disponivel) {
       console.log(`O ${titulo.nome} está disponível`);
@@ -32,18 +28,44 @@ class Biblioteca {
     }
   }
 
+  listarLivros() {
+    if (this.livros.length < 0)
+      console.log(`Não esitem livros na bilbioteca`)
+      else {
+      console.log(this.livros.length)
+      return this.livros.length}
+  }
+
   emprestarLivros(titulo) {
-    const emLeitura = !this.disponivel;
-    console.log(`O item ${titulo.nome} foi emprestado com sucesso`);
-    this.livros.pop();
-    return emLeitura;
+    let emprestado = (this.disponivel);
+    if (!emprestado)
+      console.log(`O ${titulo.nome} já está em uso`)
+    else{
+      console.log(`O ${titulo.nome} foi emprestado com sucesso`)
+    }
+    return !emprestado
+  }
+
+  devolverLivros(titulo) {
+    let emprestado = !(this.disponivel)
+      console.log(`O item ${titulo.nome} foi devolvido com sucesso`)
+    return{
+      emprestado
+    }  
   }
 }
 
 const livro1 = new Livro(`Piratas do Caribe`, 'Autor' );
+const livro2 = new Livro (`Dune`, `capeta` )
 
 const prateleira = new Biblioteca(`SEBO`);
 
 prateleira.adicionarLivros(livro1);
-// prateleira.seDisponivel(livro1);
-// prateleira.listarLivros();
+prateleira.adicionarLivros(livro2);
+prateleira.seDisponivel(livro1);
+prateleira.seDisponivel(livro2);
+
+prateleira.listarLivros()
+prateleira.emprestarLivros(livro2)
+prateleira.seDisponivel(livro2);
+prateleira.devolverLivros(livro2)
